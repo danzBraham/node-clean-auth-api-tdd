6c978778abc991bc45e43ae3ae6170e6ae947f48
Zidan Abraham <zidangit07@gmail.com>
2024-01-02 11:46:18 +0800

    refactor(eslint, use_case): improve code style and test readability

--- src/Applications/use_case/_test/AddUserUseCase.test.js
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -8,21 +8,21 @@ describe('AddUserUseCase', () => {
   it('should orchestrating the add user action correctly', async () => {
     // Arrange
     const useCasePayload = {
-      username: 'dicoding',
+      username: 'danzbraham',
       password: 'secret',
-      fullname: 'Dicoding Indonesia',
+      fullname: 'Zidan Abraham',
     };
     const mockRegisteredUser = new RegisteredUser({
       id: 'user-123',
-      username: useCasePayload.username,
-      fullname: useCasePayload.fullname,
+      username: 'danzbraham',
+      fullname: 'Zidan Abraham',
     });
 
-    /** creating dependency of use case */
+    // creating dependancy of use case
     const mockUserRepository = new UserRepository();
     const mockPasswordHash = new PasswordHash();
 
-    /** mocking needed function */
+    // mocking needed function
     mockUserRepository.verifyAvailableUsername = jest
       .fn()
       .mockImplementation(() => Promise.resolve());
@@ -33,7 +33,7 @@ describe('AddUserUseCase', () => {
       .fn()
       .mockImplementation(() => Promise.resolve(mockRegisteredUser));
 
-    /** creating use case instance */
+    // creating use case instance
     const getUserUseCase = new AddUserUseCase({
       userRepository: mockUserRepository,
       passwordHash: mockPasswordHash,
@@ -48,10 +48,10 @@ describe('AddUserUseCase', () => {
         id: 'user-123',
         username: useCasePayload.username,
         fullname: useCasePayload.fullname,
-      })
+      }),
     );
     expect(mockUserRepository.verifyAvailableUsername).toHaveBeenCalledWith(
-      useCasePayload.username
+      useCasePayload.username,
     );
     expect(mockPasswordHash.hash).toHaveBeenCalledWith(useCasePayload.password);
     expect(mockUserRepository.addUser).toHaveBeenCalledWith(
@@ -59,7 +59,7 @@ describe('AddUserUseCase', () => {
         username: useCasePayload.username,
         password: 'encrypted_password',
         fullname: useCasePayload.fullname,
-      })
+      }),
     );
   });
 });
