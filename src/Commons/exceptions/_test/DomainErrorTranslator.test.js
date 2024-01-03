e3556bdec5e612391982e4ec587d774605169dd8
Zidan Abraham <zidangit07@gmail.com>
2024-01-03 12:36:14 +0800

    feat(exceptions, tests): add DomainErrorTranslator and expand server tests

---
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -0,0 +1,29 @@
+const DomainErrorTranslator = require('../DomainErrorTranslator');
+const InvariantError = require('../InvariantError');
+
+describe('DomainErrorTranslator', () => {
+  it('should translate error correctly', () => {
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('cannot create a new user because the required properties are missing'));
+
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('cannot create a new user because the data type does not match'));
+
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_LIMIT_CHAR')))
+      .toStrictEqual(new InvariantError('cannot create a new user because the username has more than 50 characters'));
+
+    expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
+      .toStrictEqual(new InvariantError('cannot create a new user because the username contain restricted character'));
+  });
+
+  it('should return original error when error message is not needed to translate', () => {
+    // Arrange
+    const error = new Error('some_error_message');
+
+    // Action
+    const translatedError = DomainErrorTranslator.translate(error);
+
+    // Assert
+    expect(translatedError).toStrictEqual(error);
+  });
+});
