0d033d74e489a1ed1f75a23c7a492a0a31b018be
Zidan Abraham <zidangit07@gmail.com>
2024-01-04 18:55:58 +0800

    feat(domains): add placeholder verifyUserCredential method and test

--- src/Domains/user/_test/UserRepository.test.js
+++ src/Domains/user/_test/UserRepository.test.js
@@ -8,5 +8,6 @@ describe('UserRepository interface', () => {
     // Action and Assert
     await expect(userRepository.addUser({})).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(userRepository.verifyAvailableUsername('')).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyUserCredential('', '')).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
