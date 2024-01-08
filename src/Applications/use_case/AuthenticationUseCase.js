fff327e0277bfc05921ef56350520ef5b2724985
Zidan Abraham <zidangit07@gmail.com>
2024-01-08 16:16:39 +0800

    feat(authentication): implement putAuthentication use case and test

--- src/Applications/use_case/AuthenticationUseCase.js
+++ src/Applications/use_case/AuthenticationUseCase.js
@@ -5,8 +5,18 @@ class AuthenticationUseCase {
     this._tokenManager = tokenManager;
   }
 
+  _verifyGetUseCasePayload({ username, password }) {
+    if (!username || !password) {
+      throw new Error('GET_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof username !== 'string' || typeof password !== 'string') {
+      throw new Error('GET_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+
   async getAuthentication(useCasePayload) {
-    this._verifyUseCasePayload(useCasePayload);
+    this._verifyGetUseCasePayload(useCasePayload);
     const { username, password } = useCasePayload;
     const id = await this._userRepository.verifyUserCredential(username, password);
     const accessToken = await this._tokenManager.generateAccessToken({ id });
@@ -15,14 +25,23 @@ class AuthenticationUseCase {
     return { accessToken, refreshToken };
   }
 
-  _verifyUseCasePayload({ username, password }) {
-    if (!username || !password) {
-      throw new Error('GET_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY');
+  _verifyPutUseCasePayload({ refreshToken }) {
+    if (!refreshToken) {
+      throw new Error('PUT_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof username !== 'string' || typeof password !== 'string') {
-      throw new Error('GET_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    if (typeof refreshToken !== 'string') {
+      throw new Error('PUT_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
+  }
+
+  async putAuthentication(useCasePayload) {
+    this._verifyPutUseCasePayload(useCasePayload);
+    const { refreshToken } = useCasePayload;
+    await this._authenticationRepository.verifyRefreshToken(refreshToken);
+    const { id } = await this._tokenManager.verifyRefreshToken(refreshToken);
+    const accessToken = await this._tokenManager.generateAccessToken({ id });
+    return { accessToken };
   }
 }
 
--- src/Applications/use_case/_test/AuthenticationUseCase.test.js
+++ src/Applications/use_case/_test/AuthenticationUseCase.test.js
@@ -4,19 +4,19 @@ const TokenManager = require('../../token/TokenManager');
 const AuthenticationUseCase = require('../AuthenticationUseCase');
 
 describe('AuthenticationUseCase', () => {
+  // creating dependancy of use case
+  const mockAuthenticationRepository = new AuthenticationRepository();
+  const mockUserRepository = new UserRepository();
+  const mockTokenManager = new TokenManager();
+
+  // creating use case instance
+  const authenticationUseCase = new AuthenticationUseCase({
+    authenticationRepository: mockAuthenticationRepository,
+    userRepository: mockUserRepository,
+    tokenManager: mockTokenManager,
+  });
+
   describe('getAuthentication', () => {
-    // creating dependancy of use case
-    const mockAuthenticationRepository = new AuthenticationRepository();
-    const mockUserRepository = new UserRepository();
-    const mockTokenManager = new TokenManager();
-
-    // creating use case instance
-    const authenticationUseCase = new AuthenticationUseCase({
-      authenticationRepository: mockAuthenticationRepository,
-      userRepository: mockUserRepository,
-      tokenManager: mockTokenManager,
-    });
-
     it('should throw error when useCasePayload did not contain needed property', async () => {
     // Arrange
       const useCasePayload = {
@@ -38,7 +38,7 @@ describe('AuthenticationUseCase', () => {
       await expect(authenticationUseCase.getAuthentication(useCasePayload)).rejects.toThrow('GET_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION');
     });
 
-    it('should orchestrating the authentication user action correctly', async () => {
+    it('should orchestrating the get authentication user action correctly', async () => {
     // Arrange
       const useCasePayload = {
         username: 'danzbraham',
@@ -74,6 +74,52 @@ describe('AuthenticationUseCase', () => {
       expect(mockTokenManager.generateAccessToken).toHaveBeenCalledWith({ id });
       expect(mockTokenManager.generateRefreshToken).toHaveBeenCalledWith({ id });
       expect(mockAuthenticationRepository.addRefreshToken).toHaveBeenCalledWith(refreshToken);
+    });
+  });
+
+  describe('putAuthentication', () => {
+    it('should throw error when useCasePayload did not contain needed property', async () => {
+    // Arrange
+      const useCasePayload = {};
+
+      // Action and Assert
+      await expect(authenticationUseCase.putAuthentication(useCasePayload)).rejects.toThrow('PUT_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when useCasePayload did not meet data type specification', async () => {
+    // Arrange
+      const useCasePayload = { refreshToken: true };
+
+      // Action and Assert
+      await expect(authenticationUseCase.putAuthentication(useCasePayload)).rejects.toThrow('PUT_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should orchestrating the put authentication user action correctly', async () => {
+    // Arrange
+      const id = 'user-123';
+      const accessToken = 'access-token';
+      const refreshToken = 'refresh-token';
+      const useCasePayload = { refreshToken };
+
+      // mocking needed function
+      mockAuthenticationRepository.verifyRefreshToken = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve());
+      mockTokenManager.verifyRefreshToken = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve({ id }));
+      mockTokenManager.generateAccessToken = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(accessToken));
+
+      // Action
+      const authenticatedUser = await authenticationUseCase.putAuthentication(useCasePayload);
+
+      // Assert
+      expect(authenticatedUser).toStrictEqual({ accessToken });
+      expect(mockAuthenticationRepository.verifyRefreshToken).toHaveBeenCalledWith(refreshToken);
+      expect(mockTokenManager.verifyRefreshToken).toHaveBeenCalledWith(refreshToken);
+      expect(mockTokenManager.generateAccessToken).toHaveBeenCalledWith({ id });
     });
   });
 });
